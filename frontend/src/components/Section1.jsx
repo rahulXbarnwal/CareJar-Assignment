98f734e4f6cf1bcc04c8daab48e363625ec871b5
rahulXbarnwal <rahulbarnwalofficial@gmail.com>
2023-11-18 19:12:42 +0530

    api link changed

--- frontend/src/components/Section1.jsx
+++ frontend/src/components/Section1.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 
 const Section1 = () => {
   const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   const handleClick = (category) => {
@@ -16,13 +17,18 @@ const Section1 = () => {
 
   useEffect(() => {
     const getCategories = async () => {
+      setIsLoading(true);
       try {
         const res = await axios.get(
-          "https://carejar-link/api/doctor/categories"
+          "https://carejar-link.onrender.com/api/doctor/categories"
         );
-        if (res) setCategories(res.data.categories);
+        if (res) {
+          setCategories(res.data.categories);
+          setIsLoading(false);
+        }
       } catch (error) {
         alert("Error while fetching the categories!");
+        setIsLoading(false);
       }
     };
 
@@ -32,21 +38,24 @@ const Section1 = () => {
     <div className="container">
       <h2>Book an appointment for an in-clinic consultation!</h2>
       <h3>Find experienced doctors across all specialties</h3>
-      <div className="row justify-content-center">
-        {categories.map((item, index) => (
-          <div
-            className="col-md-4 col-sm-6"
-            key={index}
-            onClick={() => handleClick(item.category)}
-          >
-            <CategoryCard
-              category={item.category}
-              image={item.image}
-              description={item.description}
-            />
-          </div>
-        ))}
-      </div>
+      {isLoading && <h1>Loading...</h1>}
+      {!isLoading && (
+        <div className="row justify-content-center">
+          {categories.map((item, index) => (
+            <div
+              className="col-md-4 col-sm-6"
+              key={index}
+              onClick={() => handleClick(item.category)}
+            >
+              <CategoryCard
+                category={item.category}
+                image={item.image}
+                description={item.description}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/components/Section2.jsx
+++ frontend/src/components/Section2.jsx
@@ -8,17 +8,23 @@ import { useParams } from "react-router-dom";
 
 const Section2 = () => {
   const [doctors, setDoctors] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   let { category } = useParams();
 
   useEffect(() => {
     const getDoctors = async () => {
+      setIsLoading(true);
       try {
         const res = await axios.get(
-          `https://carejar-link/api/doctor/${category}`
+          `https://carejar-link.onrender.com/api/doctor/${category}`
         );
-        if (res) setDoctors(res.data.doctors);
+        if (res) {
+          setDoctors(res.data.doctors);
+          setIsLoading(false);
+        }
       } catch (error) {
         alert("Error while loading doctors data!");
+        setIsLoading(false);
       }
     };
     getDoctors();
@@ -26,20 +32,22 @@ const Section2 = () => {
 
   return (
     <div className="container">
-      {doctors.map((item, index) => (
-        <DoctorCard
-          key={index}
-          name={item.name}
-          image={item.image}
-          type={item.type}
-          experience={item.experience}
-          address={item.address}
-          clinicName={item.clinicName}
-          consultationFee={item.consultationFee}
-          rating={item.rating}
-          patientStories={item.patientStories}
-        />
-      ))}
+      {isLoading && <h1>Loading...</h1>}
+      {!isLoading &&
+        doctors.map((item, index) => (
+          <DoctorCard
+            key={index}
+            name={item.name}
+            image={item.image}
+            type={item.type}
+            experience={item.experience}
+            address={item.address}
+            clinicName={item.clinicName}
+            consultationFee={item.consultationFee}
+            rating={item.rating}
+            patientStories={item.patientStories}
+          />
+        ))}
     </div>
   );
 };
