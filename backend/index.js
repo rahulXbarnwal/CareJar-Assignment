9f2369186e0e10083d6622902744a136e9baef43
rahulXbarnwal <rahulbarnwalofficial@gmail.com>
2023-11-18 19:15:30 +0530

    allowing every domains

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,7 @@ const cors = require("cors");
 const app = express();
 const doctorRoute = require("./routes/doctor");
 
-const corsOptions = {
-  origin: "http://carejar-assignment.com",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/doctor", doctorRoute);
